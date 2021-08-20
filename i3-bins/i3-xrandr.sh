b1b7bd1413eabd7855e54463cc09e4ad7b32108e
Hugo Posca <hugo@gruntwork.io>
2021-08-20 09:31:24 -0700

    Rotating monitor

--- i3-bins/i3-xrandr.sh
+++ i3-bins/i3-xrandr.sh
@@ -4,6 +4,5 @@ xrandr \
   --output DP-1 --off \
   --output DP-2 --mode 1920x1080 --pos 0x0 --rotate normal \
   --output DP-3 --mode 2560x1440 --pos 1920x0 --rotate normal \
-  --output HDMI-0 --mode 1920x1080 --pos 4480x0 --rotate normal \
+  --output HDMI-0 --mode 1920x1080 --pos 4480x0 --rotate left \
   --output DP-4 --off
-
