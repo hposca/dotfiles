22cecf8c6676d8487f5c47d33be365c8acce6c7e
Hugo Posca <hugo@gruntwork.io>
2021-10-20 13:04:21 -0700

    Put bar at the bottom on the right monitor

--- i3-bins/i3-launch-polybar.sh
+++ i3-bins/i3-launch-polybar.sh
@@ -5,14 +5,21 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
-# Launch example bar
+# Launch bars
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example 2>&1 | tee -a /tmp/polybarexample-$m.log & disown
+    if [[ m -eq "HDMI-0" ]]; then
+      MONITOR=$m polybar --reload bottom 2>&1 | tee -a /tmp/polybarbottom-$m.log &
+      disown
+    else
+      MONITOR=$m polybar --reload main 2>&1 | tee -a /tmp/polybarmain-$m.log &
+      disown
+    fi
   done
 else
-  echo "---" | tee -a /tmp/polybarexample.log
-  polybar --reload example 2>&1 | tee -a /tmp/polybarexample.log & disown
+  echo "---" | tee -a /tmp/polybarmain.log
+  polybar --reload main 2>&1 | tee -a /tmp/polybarmain.log &
+  disown
 fi
 
 echo "Bars launched..."
