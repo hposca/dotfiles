731f5dc3d60f638552bd37c607a1c1413e9ca7bb
Hugo Posca <hugo@gruntwork.io>
2021-11-19 16:22:22 -0800

    Fix monitors positions

--- i3-bins/i3-xrandr.sh
+++ i3-bins/i3-xrandr.sh
@@ -2,7 +2,7 @@
 xrandr \
   --output DP-0 --off \
   --output DP-1 --off \
-  --output DP-2 --mode 1920x1080 --pos 0x0 --rotate normal \
-  --output DP-3 --mode 2560x1440 --pos 1920x0 --rotate normal \
+  --output DP-2 --mode 1920x1080 --pos 0x840 --rotate normal \
+  --output DP-3 --mode 2560x1440 --pos 1920x480 --rotate normal \
   --output HDMI-0 --mode 1920x1080 --pos 4480x0 --rotate left \
   --output DP-4 --off
