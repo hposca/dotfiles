21bc77b2206f35217f78021d0bc4571e6d62bbe4
Hugo Posca <hugo.posca@gmail.com>
2023-03-02 17:43:59 -0800

    Improve listchars to display tabs and spaces

--- config.lua
+++ config.lua
@@ -388,7 +388,7 @@ vim.g["seoul256_background"] = 236
 vim.g["seoul256_srgb"] = 1
 vim.opt.showmode = true
 vim.opt.relativenumber = true
-vim.opt.listchars = { eol = "$", tab = "→ ", trail = "␣", extends = "…" }
+vim.opt.listchars = { eol = "$", tab = "→ →", trail = "␣", extends = "…", space = "·" }
 vim.opt.clipboard = ""
 -- vim.g["vimwiki_list"] = "[{'path': '~/vimwiki/', 'syntax': 'markdown', 'ext': '.md'}]"
 -- vim.g["vimwiki_ext2syntax"] =
