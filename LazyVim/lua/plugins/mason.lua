657f64fa33da521bacfb6605e525dd00ee10e0fd
Hugo Posca <hugo.posca@gmail.com>
2023-05-02 16:26:41 -0700

    [LazyVim] LSP/Formatters installed by default

---
+++ LazyVim/lua/plugins/mason.lua
@@ -0,0 +1,199 @@
+return {
+	"williamboman/mason.nvim",
+	opts = {
+		ensure_installed = {
+			-- #ANSIBLE
+			"ansible-language-server",
+			"ansible-lint",
+
+			-- #AWK
+			"awk-language-server",
+
+			-- #BASH
+			"bash-debug-adapter",
+			"bash-language-server",
+			"beautysh",
+			"shellcheck",
+			"shellharden",
+			"shfmt",
+
+			-- #DHALL
+			"dhall-lsp",
+
+			-- DOCKER
+			"docker-compose-language-service",
+			"dockerfile-language-server",
+			"hadolint",
+
+			-- #DOT
+			"dot-language-server",
+
+			-- #GO
+			"delve",
+			"goimports",
+			"golangci-lint",
+			"gopls",
+			-- "djlint",
+			-- "go-debug-adapter",
+			-- "gofumpt",
+			-- "goimports-reviser",
+			-- "golangci-lint-langserver",
+			-- "golines",
+			-- "gomodifytags",
+			-- "gospel",
+			-- "gotests",
+			-- "gotestsum",
+			-- "iferr",
+			-- "impl",
+			-- "json-to-struct",
+			-- "revive",
+			-- "semgrep",
+			-- "sonarlint-language-server",
+			-- "staticcheck",
+
+			-- #HELM
+			"helm-ls",
+
+			-- #JINJA
+			-- "curlylint",
+			-- "djlint",
+
+			-- #JSON
+			-- "cfn-lint",
+			-- "clang-format",
+			-- "fixjson",
+			"jq",
+			"jsonlint",
+			-- "nxls",
+			-- "prettier",
+			-- "prettierd",
+			-- "rome",
+			-- "semgrep",
+			-- "spectral-language-server",
+			"json-lsp",
+
+			-- #JSONNET
+			-- "jsonnet-language-server",
+
+			-- #JQ
+			"jq-lsp",
+
+			-- #LATEX
+			-- "latexindent",
+			-- "ltex-ls ltex",
+			-- "tectonic",
+			-- "texlab",
+			-- "vale",
+
+			-- #LUA
+			"lua-language-server",
+			"stylua",
+			-- "lemmy-help",
+			-- "luacheck",
+			-- "luaformatter",
+			-- "selene",
+
+			-- #MARKDOWN
+			"markdownlint",
+			"marksman",
+			-- "alex",
+			-- "cbfmt",
+			-- "glow",
+			-- "grammarly-languageserver",
+			-- "ltex-ls",
+			-- "markdown-toc",
+			-- "prettier",
+			-- "prettierd",
+			-- "proselint",
+			-- "prosemd-lsp",
+			-- "remark-cli",
+			-- "remark-language-server",
+			-- "rome",
+			-- "textlint",
+			-- "vale",
+			-- "write-good",
+			-- "zk",
+
+			-- #OpenAPI
+			-- "vacuum",
+
+			-- #Protobuf
+			-- "buf",
+			-- "buf-language-server",
+			-- "protolint",
+
+			-- #PYTHON
+			"black",
+			"pyright",
+			-- "autoflake",
+			-- "autopep8",
+			-- "blackd-client",
+			-- "blue",
+			-- "debugpy",
+			-- "flake8",
+			-- "isort",
+			-- "jedi-language-server",
+			-- "mypy",
+			-- "pydocstyle",
+			-- "pyflakes",
+			-- "pylama",
+			-- "pylint",
+			-- "pylyzer",
+			-- "pyproject-flake8",
+			-- "pyre",
+			-- "python-lsp-server",
+			-- "reorder-python-imports",
+			-- "rstcheck",
+			-- "ruff",
+			-- "ruff-lsp",
+			-- "semgrep",
+			-- "sonarlint-language-server",
+			-- "sourcery",
+			-- "usort",
+			-- "vulture",
+			-- "yapf",
+
+			-- #RUBY
+			-- "erb-lint",
+			-- "htmlbeautifier",
+			-- "rubocop",
+			-- "ruby-lsp",
+			-- "semgrep",
+			-- "solargraph",
+			-- "sorbet",
+			-- "standardrb",
+
+			-- #SQL
+			-- "sql-formatter",
+			-- "sqlfluff",
+			-- "sqlfmt",
+			-- "sqlls",
+			-- "sqls",
+
+			-- #TOML
+			-- "taplo",
+
+			-- #TERRAFORM
+			-- "sonarlint-language-server",
+			"terraform-ls",
+			"tflint",
+			"tfsec",
+
+			-- #XML
+			-- "lemminx",
+			-- "xmlformatter",
+
+			-- #YAML
+			"yaml-language-server",
+			-- "actionlint",
+			-- "cfn-lint",
+			-- "prettier",
+			-- "prettierd",
+			-- "spectral-language-server",
+			-- "yamlfix",
+			-- "yamlfmt",
+			-- "yamllint",
+			-- "yq",
+		},
+	},
+}
