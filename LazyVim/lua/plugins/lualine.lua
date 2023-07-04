7721264bb886869ee0677160ccd49c34c017b8f3
Hugo Posca <hugo.posca@gmail.com>
2023-07-04 13:50:50 -0700

    [LazyVim] Lualine - filenames on bottom, navic on top
    Updated LazyVim's lualine bottom part to not display navic.
    Basically removing navic from [1].
    
    [1]: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/ui.lua

--- LazyVim/lua/plugins/lualine.lua
+++ LazyVim/lua/plugins/lualine.lua
@@ -1,9 +1,25 @@
 local navic = require("nvim-navic")
+local icons = require("lazyvim.config").icons
 
 return {
 	{
 		"nvim-lualine/lualine.nvim",
 		opts = {
+			sections = {
+				lualine_c = {
+					{
+						"diagnostics",
+						symbols = {
+							error = icons.diagnostics.Error,
+							warn = icons.diagnostics.Warn,
+							info = icons.diagnostics.Info,
+							hint = icons.diagnostics.Hint,
+						},
+					},
+					{ "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+					{ "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
+				},
+			},
 			winbar = {
 				lualine_c = {
 					{
