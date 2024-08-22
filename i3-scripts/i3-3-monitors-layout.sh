c6cab0480c8e2d3c6446f5da6f1e1a48827d5beb
Hugo Posca <hugo.posca@gmail.com>
2024-08-22 09:56:34 -0700

    [i3] Multi monitor configuration

--- i3-scripts/i3-3-monitors-layout.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --mode 1920x1080 --pos 4480x0 --rotate left --output DP-2 --mode 1920x1080 --pos 0x840 --rotate normal --output DP-3 --mode 2560x1440 --pos 1920x480 --rotate normal --output DP-4 --off
