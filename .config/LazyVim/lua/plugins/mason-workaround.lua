d0c44f4c364abb004b2bb2d9b6cc127fc59871e0
Hugo Posca <hugo.posca@gmail.com>
2025-10-07 13:42:59 -0700

    [LazyVim] Remove mason workaround
    As per the initial thread,
    https://github.com/LazyVim/LazyVim/issues/6039\#issuecomment-3341700717
    It can be removed now :)

--- .config/LazyVim/lua/plugins/mason-workaround.lua
+++
@@ -1,7 +0,0 @@
--- https://github.com/LazyVim/LazyVim/issues/6039
--- https://github.com/LazyVim/LazyVim/issues/6039#issuecomment-2858811111
--- https://github.com/LazyVim/LazyVim/issues/6039#issuecomment-2859309755
-return {
-   { "mason-org/mason.nvim", version = "^1.0.0" },
-   { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
- }
