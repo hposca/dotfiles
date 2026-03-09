8bc162eff8ddfc1cf7e8079c7756d61f7e0d4c13
Hugo Posca <hugo.posca@gmail.com>
2026-03-09 12:59:39 -0700

    [LazyVim] Update obsidian daily alias format to include the weekday

--- .config/LazyVim/lua/plugins/plugins.lua
+++ .config/LazyVim/lua/plugins/plugins.lua
@@ -257,7 +257,7 @@ return {
 				-- Optional, if you want to change the date format for the ID of daily notes.
 				date_format = "%Y-%m-%d",
 				-- Optional, if you want to change the date format of the default alias of daily notes.
-				alias_format = "%B %-d, %Y",
+				alias_format = "%B %-d, %Y - %A",
 				-- Optional, default tags to add to each new daily note created.
 				default_tags = { "daily-notes" },
 				-- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
